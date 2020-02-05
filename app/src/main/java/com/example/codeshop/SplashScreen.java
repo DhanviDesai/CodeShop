55e2a11ad482e88d8487a763fe8195e8f82ac7dd
DhanviDesai <dhanvidesai99@gmail.com>
2020-02-05 12:50:23 +0530

    Ui Changes2

---
+++ app/src/main/java/com/example/codeshop/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.example.codeshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(i);
+                overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+            }
+        },2000);
+    }
+}
